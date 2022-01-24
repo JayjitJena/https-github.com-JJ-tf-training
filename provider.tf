ffdfc3b46146cdc1737b290390c3b0643500f73e
Nana <nana.oppong.quayson@accenture.com>
2022-01-24 16:40:38 -0500

    update provider with access

--- provider.tf
+++ provider.tf
@@ -7,6 +7,16 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region  = var.region
+  profile = "nana"
 }
+
+
+
+# provider "aws" {
+#   region     = "us-west-2"
+#   access_key = "my-access-key"
+#   secret_key = "my-secret-key"
+# }
